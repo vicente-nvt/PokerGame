623468b23bf97abfe1911366c147cd9975a4fd6e
José Vicente Neto <joseneto@digix.com.br>
2018-06-19 14:58:19 -0400

    Adicionando jogada Quadra

---
+++ PokerGame.Dominio/Jogadas/Quadra.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PokerGame.Dominio.Jogadas
+{
+    public class Quadra : IJogada
+    {
+        private IList<Carta> _maoDe5Cartas;
+
+        public Quadra(IList<Carta> maoDe5Cartas)
+        {
+            _maoDe5Cartas = maoDe5Cartas;
+        }
+
+        public List<Carta> Encontrar()
+        {
+            var quadra = new List<Carta>();
+
+            foreach (var carta in _maoDe5Cartas)
+            {
+                var outrasTresCartasComMesmoValor = _maoDe5Cartas.Where(outraCarta =>
+                    outraCarta.Valor == carta.Valor && outraCarta.HashDaCarta != carta.HashDaCarta).ToList();
+
+                if (outrasTresCartasComMesmoValor.Count == 3)
+                {
+                    quadra.Add(carta);
+                    quadra.AddRange(outrasTresCartasComMesmoValor);
+                    break;
+                }
+            }
+
+            return quadra;
+        }
+
+        public bool JogadaEncontradaNaMao() => Encontrar().Count == 4;
+    }
+}
---
+++ PokerGame.Testes/Jogadas/QuadraTeste.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PokerGame.Dominio;
+using PokerGame.Dominio.Builders;
+using PokerGame.Dominio.Jogadas;
+using Xunit;
+
+namespace PokerGame.Testes.Jogadas
+{
+    public class QuadraTeste
+    {
+        private IList<Carta> _maoDe5Cartas;
+
+        public QuadraTeste()
+        {
+            _maoDe5Cartas = new List<Carta>
+            {
+                CartaBuilder.UmaCarta().ComValor(5).ComNaipe(Naipes.Espadas).Construir(),
+                CartaBuilder.UmaCarta().ComValor(5).ComNaipe(Naipes.Ouros).Construir(),
+                CartaBuilder.UmaCarta().ComValor(5).ComNaipe(Naipes.Paus).Construir(),
+                CartaBuilder.UmaCarta().ComValor(5).ComNaipe(Naipes.Copas).Construir(),
+                CartaBuilder.UmaCarta().ComValor(6).ComNaipe(Naipes.Ouros).Construir()
+            };
+        }
+
+        [Fact]
+        public void DeveEncontrarUmaQuadraNaMao()
+        {
+            var quadraEsperada = new List<Carta>
+            {
+                CartaBuilder.UmaCarta().ComValor(5).ComNaipe(Naipes.Espadas).Construir(),
+                CartaBuilder.UmaCarta().ComValor(5).ComNaipe(Naipes.Ouros).Construir(),
+                CartaBuilder.UmaCarta().ComValor(5).ComNaipe(Naipes.Paus).Construir(),
+                CartaBuilder.UmaCarta().ComValor(5).ComNaipe(Naipes.Copas).Construir(),
+            }.Select(carta => carta.HashDaCarta).ToList();
+
+            var quadraEncontrada = new Quadra(_maoDe5Cartas).Encontrar().Select(carta => carta.HashDaCarta).ToList();
+            
+            Assert.Equal(quadraEsperada, quadraEncontrada);
+        }
+
+        [Fact]
+        public void DeveEncontrarAJogadaNaMao()
+        {
+            var jogadaEncontradaNaMao = new Quadra(_maoDe5Cartas).JogadaEncontradaNaMao();
+
+            Assert.True(jogadaEncontradaNaMao);
+        }
+
+        [Fact]
+        public void NaoDeveEncontrarAJogadaNaMaoSeNaoHouverUmaQuadra()
+        {
+            _maoDe5Cartas[0] = CartaBuilder.UmaCarta().ComValor(14).ComNaipe(Naipes.Ouros).Construir();
+
+            var jogadaEncontradaNaMao = new Quadra(_maoDe5Cartas).JogadaEncontradaNaMao();
+
+            Assert.False(jogadaEncontradaNaMao);
+        }
+    }
+}
