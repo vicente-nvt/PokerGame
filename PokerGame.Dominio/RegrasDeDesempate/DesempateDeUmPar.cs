6f1fe5519860eccdb3e145b8e8e2bf23e9cf73ce
José Vicente Neto <joseneto@digix.com.br>
2018-07-13 15:50:09 -0400

    Adicionando regra de desempate para Um par

---
+++ PokerGame.Dominio/RegrasDeDesempate/DesempateDeUmPar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PokerGame.Dominio.Identificadores;
+
+namespace PokerGame.Dominio.RegrasDeDesempate
+{
+    public class DesempateDeUmPar : IRegraDeDesempate
+    {
+        private readonly IIDentificadorDeCartas _identificadorDePar;
+        private readonly IIDentificadorDeCartas _identificadorDeCartaMaisAlta;
+
+        public DesempateDeUmPar(IIDentificadorDeCartas identificadorDePar, 
+            IIDentificadorDeCartas identificadorDeCartaMaisAlta)
+        {
+            _identificadorDePar = identificadorDePar;
+            _identificadorDeCartaMaisAlta = identificadorDeCartaMaisAlta;
+        }
+
+        public List<Carta> Desempatar(List<Carta> maoA, List<Carta> maoB)
+        {
+            var parDaMaoA = _identificadorDePar.IdentificarCartas(maoA);
+            var parDaMaoB = _identificadorDePar.IdentificarCartas(maoB);
+
+            var restanteDaMaoA = maoA.Where(carta => !parDaMaoA.Contains(carta)).ToList();
+            var restanteDaMaoB = maoB.Where(carta => !parDaMaoB.Contains(carta)).ToList();
+
+            if (parDaMaoA.First().Valor != parDaMaoB.First().Valor)
+                return parDaMaoA.First().Valor > parDaMaoB.First().Valor ? maoA : maoB;
+
+            Carta cartaMaisAltaDaMaoA;
+            Carta cartaMaisAltaDaMaoB;
+
+            do
+            {
+                cartaMaisAltaDaMaoA = _identificadorDeCartaMaisAlta.IdentificarCartas(restanteDaMaoA).First();
+                cartaMaisAltaDaMaoB = _identificadorDeCartaMaisAlta.IdentificarCartas(restanteDaMaoB).First();
+
+                restanteDaMaoA.Remove(cartaMaisAltaDaMaoA);
+                restanteDaMaoB.Remove(cartaMaisAltaDaMaoB);
+
+                if (restanteDaMaoA.Count == 0 || restanteDaMaoB.Count == 0)
+                    return new List<Carta>();
+
+            } while (cartaMaisAltaDaMaoB.Valor == cartaMaisAltaDaMaoA.Valor);
+
+            return cartaMaisAltaDaMaoA.Valor > cartaMaisAltaDaMaoB.Valor ? maoA : maoB;
+        }
+    }
+}
---
+++ PokerGame.Testes/RegrasDeDesempate/DesempateDeUmParTeste.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using PokerGame.Dominio;
+using PokerGame.Dominio.Builders;
+using PokerGame.Dominio.Identificadores;
+using PokerGame.Dominio.RegrasDeDesempate;
+using Xunit;
+
+namespace PokerGame.Testes.RegrasDeDesempate
+{
+    public class DesempateDeUmParTeste
+    {
+        private List<Carta> _maoA;
+        private List<Carta> _maoB;
+        private readonly DesempateDeUmPar _desempateDeUmPar;
+
+        public DesempateDeUmParTeste()
+        {
+            var identificadorDePar = new IdentificaDuasCartasComValoresIguais();
+            var identificadorDeCartaMaisAlta = new IdentificaCartaMaisAlta();
+            _maoA = new List<Carta>
+            {
+                CartaBuilder.UmaCarta().ComValor(10).ComNaipe(Naipes.Ouros).Construir(),
+                CartaBuilder.UmaCarta().ComValor(10).ComNaipe(Naipes.Espadas).Construir(),
+                CartaBuilder.UmaCarta().ComValor(2).ComNaipe(Naipes.Ouros).Construir(),
+                CartaBuilder.UmaCarta().ComValor(6).ComNaipe(Naipes.Paus).Construir(),
+                CartaBuilder.UmaCarta().ComValor(9).ComNaipe(Naipes.Copas).Construir(),
+            };
+            _maoB = new List<Carta>
+            {
+                CartaBuilder.UmaCarta().ComValor(8).ComNaipe(Naipes.Ouros).Construir(),
+                CartaBuilder.UmaCarta().ComValor(8).ComNaipe(Naipes.Espadas).Construir(),
+                CartaBuilder.UmaCarta().ComValor(3).ComNaipe(Naipes.Ouros).Construir(),
+                CartaBuilder.UmaCarta().ComValor(4).ComNaipe(Naipes.Paus).Construir(),
+                CartaBuilder.UmaCarta().ComValor(5).ComNaipe(Naipes.Copas).Construir(),
+            };
+            _desempateDeUmPar = new DesempateDeUmPar(identificadorDePar, identificadorDeCartaMaisAlta);
+        }
+
+        [Fact]
+        public void DeveDesempatarEntreDoisParesDiferentes()
+        {
+            var maoVencedoraEsperada = _maoA.Select(carta => carta.HashDaCarta).ToList();
+            
+            var maoVencedoraEncontrada = _desempateDeUmPar.Desempatar(_maoA, _maoB)
+                .Select(carta => carta.HashDaCarta).ToList();
+
+            Assert.Equal(maoVencedoraEsperada, maoVencedoraEncontrada);
+        }
+
+        [Fact]
+        public void DeveDesempatarEntreDoisParesIguais()
+        {
+            _maoA = _maoB.Select(carta => carta).ToList();
+            _maoA[4] = CartaBuilder.UmaCarta().ComValor(12).ComNaipe(Naipes.Copas).Construir();
+
+            var maoVencedoraEsperada = _maoA.Select(carta => carta.HashDaCarta).ToList();
+
+            var maoVencedoraEncontrada = _desempateDeUmPar.Desempatar(_maoA, _maoB)
+                .Select(carta => carta.HashDaCarta).ToList();
+
+            Assert.Equal(maoVencedoraEsperada, maoVencedoraEncontrada);
+        }
+
+        [Fact]
+        public void NaoDeveEncontrarVencedorEntreDuasMaosIguais()
+        {
+            _maoB = _maoA.Select(carta => carta).ToList();
+
+            var maoVencedoraEncontrada = _desempateDeUmPar.Desempatar(_maoA, _maoB);
+
+
+            Assert.Empty(maoVencedoraEncontrada);
+        }
+    }
+}
